d85e5bda6e23b3461cd69d9f041cb6e3767ad3c8
Razvan Tomegea <razvantomegea@users.noreply.github.com>
2019-12-21 13:56:37 +0200

    Updated firebase config

--- src/app/core/firebase/config.ts
+++ src/app/core/firebase/config.ts
@@ -1,8 +1,10 @@
 export const FIREBASE_CONFIG = {
-  apiKey: 'AIzaSyCE0s-CLtOHt3QLDR3KKroUD8LCw3WlJ9M',
-  authDomain: 'angularfire-material-dashboard.firebaseapp.com',
-  databaseURL: 'https://angularfire-material-dashboard.firebaseio.com',
-  projectId: 'angularfire-material-dashboard',
-  storageBucket: 'angularfire-material-dashboard.appspot.com',
-  messagingSenderId: '229877504734'
+  apiKey: "AIzaSyAwJ7daCQay0UIsHMq8RUzVscTK9Xdduks",
+  authDomain: "sanofit-891ab.firebaseapp.com",
+  databaseURL: "https://sanofit-891ab.firebaseio.com",
+  projectId: "sanofit-891ab",
+  storageBucket: "sanofit-891ab.appspot.com",
+  messagingSenderId: "488564822617",
+  appId: "1:488564822617:web:79a85ea7a0288e8c5fd748",
+  measurementId: "G-P3LGVZC9ZH"
 };
