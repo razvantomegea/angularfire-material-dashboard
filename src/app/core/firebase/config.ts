d8ca960312b0c1fdee19a5d7b7cce2dd7887a39c
Razvan Tomegea <razvantomegea@users.noreply.github.com>
2019-12-21 14:10:31 +0200

    Removed firebase config

--- src/app/core/firebase/config.ts
+++ src/app/core/firebase/config.ts
@@ -1,10 +1,1 @@
-export const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyAwJ7daCQay0UIsHMq8RUzVscTK9Xdduks",
-  authDomain: "sanofit-891ab.firebaseapp.com",
-  databaseURL: "https://sanofit-891ab.firebaseio.com",
-  projectId: "sanofit-891ab",
-  storageBucket: "sanofit-891ab.appspot.com",
-  messagingSenderId: "488564822617",
-  appId: "1:488564822617:web:79a85ea7a0288e8c5fd748",
-  measurementId: "G-P3LGVZC9ZH"
-};
+export const FIREBASE_CONFIG = {};
